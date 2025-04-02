445653d4551bddc043a9b1c1c71e9fa1b33d0bc0
Gavin <gsstockwell@gmail.com>
2025-04-01 21:17:44 -0700

    add update, needs fixing, also added the update client

---
+++ client/src/pages/Updateemoji.tsx
@@ -0,0 +1,160 @@
+import { useState, type FormEvent, type ChangeEvent } from 'react';
+
+import { useMutation, useQuery } from '@apollo/client';
+import { UPDATE_EMOJI } from '../utils/mutations';
+import { QUERY_LAST_EMOJI } from '../utils/queries';
+
+import { useParams } from 'react-router-dom';
+
+const Updateemoji = () => {
+  const [formState, setFormState] = useState({
+    emojiText: '',
+    emojiDescription: '',
+    emojiAuthor: '',
+    emojiId:'',
+  });
+
+  const [addEmoji, { error, data: mutationData }] = useMutation(UPDATE_EMOJI);
+
+  const { username: userParam } = useParams();
+  const { loading, data, refetch } = useQuery(QUERY_LAST_EMOJI, {
+    variables: { username: userParam },
+  });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addEmoji({
+        variables: { input: { ...formState } },
+      });
+
+      console.log('Emoji added:', data);
+
+      // Refetch the latest emoji data
+      await refetch();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <main className="flex-row justify-center mb-4">
+      <div className="col-12 col-lg-10">
+        <div className="card">
+          <h4 className="card-header bg-dark text-light p-2">Add Emoji!</h4>
+          <div className="card-body">
+            {mutationData ? (
+              <form onSubmit={handleFormSubmit}>
+                <p>Your emoji has been added {data.lastEmoji.emojiText}</p>
+
+                <input
+                  className="form-input"
+                  placeholder="Emoji Name"
+                  name="emojiText"
+                  type="text"
+                  value={formState.emojiText}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="Emoji Description"
+                  name="emojiDescription"
+                  type="text"
+                  value={formState.emojiDescription}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="Emoji Author"
+                  name="emojiAuthor"
+                  type="text"
+                  value={formState.emojiAuthor}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="Emoji ID"
+                  name="emojiId"
+                  type="text"
+                  value={formState.emojiId}
+                  onChange={handleChange}
+                />
+                <button
+                  className="btn btn-block btn-primary"
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+            ) : (
+              <form onSubmit={handleFormSubmit}>
+                <input
+                  className="form-input"
+                  placeholder="Emoji Name"
+                  name="emojiText"
+                  type="text"
+                  value={formState.emojiText}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="Emoji Description"
+                  name="emojiDescription"
+                  type="text"
+                  value={formState.emojiDescription}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="Emoji Author"
+                  name="emojiAuthor"
+                  type="text"
+                  value={formState.emojiAuthor}
+                  onChange={handleChange}
+                />
+                <input
+                  className="form-input"
+                  placeholder="Emoji ID"
+                  name="emojiId"
+                  type="text"
+                  value={formState.emojiId}
+                  onChange={handleChange}
+                />
+                <button
+                  className="btn btn-block btn-primary"
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
+                  Submit
+                </button>
+              </form>
+            )}
+
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Updateemoji;
